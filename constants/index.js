ed3d534907fbf243c6663b893f82d58a5fbbc712
Honey <137643427+honeylocanto@users.noreply.github.com>
2023-07-31 21:37:45 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfDac24eCA99000Fbf87e7cCA74dF2A112AeAf05a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
